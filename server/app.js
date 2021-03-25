661bc2912cd6a027c82b32367dcde338518fe7d8
Sarunyou Whangbunyapirat <sarunyou.whangbunyapirat@ifyoucan.com>
2021-03-26 00:07:41 +0700

    fix: connect db

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 var express = require("express");
+var cors = require("cors");
+require("dotenv-safe").config();
+// Databases
+require("./db/sequelize");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const routerV1 = require("./routes/v1/index");
 
 var app = express();
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- server/models/blog.model.js
+++ server/models/blog.model.js
@@ -1,41 +1,41 @@
 module.exports = (sequelize, DataTypes) => {
-	const Blog = sequelize.define(
-		'blog',
-		{
-			id: {
-				type: DataTypes.STRING(50),
-				allowNull: false,
-				primaryKey: true,
-			},
-			name: {
-				type: DataTypes.STRING(100),
-				allowNull: false,
-			},
-			status: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-			},
-			content: {
-				type: DataTypes.STRING(100),
-				allowNull: false,
-			},
-			category: {
-				type: DataTypes.STRING(100),
-				allowNull: false,
-			},
-			user_id: {
-				type: DataTypes.STRING(100),
-				allowNull: false,
-			},
-		},
-		{
-			// Other model options go here
-		}
-	);
+  const Blog = sequelize.define(
+    "blog",
+    {
+      id: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      status: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      category: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      user_id: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+    },
+    {
+      // Other model options go here
+    }
+  );
 
-	Blog.associate = function (models) {
-		Blog.belongsTo(models.User, { as: 'author', foreignKey: 'user_id'})
-	}
+  Blog.associate = function (models) {
+    Blog.belongsTo(models.user, { as: "author", foreignKey: "user_id" });
+  };
 
-	return Blog;
+  return Blog;
 };
